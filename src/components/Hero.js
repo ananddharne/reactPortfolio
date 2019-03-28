80a35224459c8b08b661903cb8b2da48438bb2af
Anand Dharne <anand.dharne24@gmail.com>
2019-03-28 16:12:06 -0400

    missed dot

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,7 @@ class Hero extends React.PureComponent {
       <div className="hero-container">
         <Typewriter className="hero-title" text="Anand Dharne" />
         <div className="hero-description">
-          I am a web developer leaning towards full stack development at <a href="https://www.gethappie.me/"> Happie</a> My passion is building simple,
+          I am a web developer leaning towards full stack development at <a href="https://www.gethappie.me/"> Happie.</a> My passion is building simple,
           beautiful user experiences. At my current job, I work on all stages of the SDLC lifecycle; as part of QA automation, setting up the test environment, writing tests and handling CI/CD pipelines. Work with dockers and sporadically with gcr(google cloud registry) to maintain and configure images. I am proficient with Linux, predominantly Arch and Ubuntu. <br />Check out my{' '}
           <Scrollchor to="#projects">projects</Scrollchor> below.
         </div>
