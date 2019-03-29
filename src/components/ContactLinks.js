214951a3a9cc33833e4550dac37c8183cb0120b2
Anand Dharne <anand.dharne24@gmail.com>
2019-03-29 02:25:48 -0400

    fix syntax n links

--- src/components/ContactLinks.js
+++ src/components/ContactLinks.js
@@ -13,13 +13,13 @@ class ContactLinks extends React.PureComponent {
           <a
             rel="noopener"
             target="_blank"
-            href="https://www.linkedin.com/in/justin-chi-64b12b37"
+            href="https://www.linkedin.com/in/ananddharne/"
           >
             LinkedIn
           </a>
         </span>
         <span>
-          <a rel="noopener" target="_blank" href="https://github.com/vai0">
+          <a rel="noopener" target="_blank" href="https://github.com/ananddharne">
             github
           </a>
         </span>
