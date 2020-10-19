ec27196a136f515311c9a72fab22b1554fd116d5
anand.dharne24@gmail.com <anand@gethappie.me>
2020-10-19 00:23:41 -0400

    description edit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -15,7 +15,7 @@ class Hero extends React.PureComponent {
         angle: 90,
         spread: 960,
         startVelocity: 60,
-        elementCount: 20,
+        elementCount: 30,
         dragFriction: 0.12,
         duration: 3000,
         stagger: 3,
@@ -36,26 +36,21 @@ class Hero extends React.PureComponent {
   changeBackground(e) {
     this.setState({text: ' break'})
     this.setState({completed: true})
-    // this.headingText.style.display = 'none'
-    // const elem = document.getElementsByClassName('hero-title')
-    // elem[0].style.display = 'none'
-    e.target.innerHTML = 'I BREAK THEM'
+    e.target.innerHTML = 'I BREAK THINGS'
     const elem1 = document.getElementsByClassName('make-break')
     elem1[0].innerHTML = ''
     console.log(e.target)
-    // e.stopPropogation()
   }
 
   changebg(e) {
     this.setState({text: ' make'})
     this.setState({completed: false})
     e.target.innerHTML = 'ANAND DHARNE'
-    // const elem = document.getElementsByClassName('hero-title')
-    setTimeout(() => {
+    // setTimeout(() => {
     // elem[0].style.display = 'block'
     const elem1 = document.getElementsByClassName('make-break')
     elem1[0].innerHTML = 'I MAKE THINGS'
-    },250)
+    // },250)
   
   }
 
@@ -63,14 +58,16 @@ class Hero extends React.PureComponent {
     return (
       <div className="hero-container">
         <Confetti active={ this.state.completed } config={this.state.config}/>
-        <div ref={this.headingText} id="s" onMouseOver={this.changeBackground} onMouseLeave={this.changebg}>
+        <div ref={this.headingText} onMouseOver={this.changeBackground} onMouseLeave={this.changebg}>
         <Typewriter className="hero-title" headingText={this.state.headingTexts} />
         </div>
         <br></br>
-        <span className='make-break' style={{fontSize: '40px', textAlign: 'center', color: "#747fe0" }}> <b>I MAKE THINGS</b></span>
+        <span className='make-break'> <b>I MAKE THINGS</b></span>
         <div className="hero-description">
-          I am a web developer leaning towards full stack development at <a href="https://www.gethappie.me/"> Happie.</a> My passion is building simple,
-          beautiful user experiences. At my current job, I work on all stages of the SDLC lifecycle; as part of QA automation, setting up the test environment, writing tests and handling CI/CD pipelines. Work with dockers and sporadically with gcr(google cloud registry) to maintain and configure images. I am proficient with Linux, predominantly Arch and Ubuntu. <br />Check out my{' '}
+          I am a software professional at <a target="_blank" href="https://www.sessionm.com/"> SessionM.</a>  At my current job, I work on all stages of the SDLC lifecycle; as part of automation, setting up the test environment, developing automation frameworks, writing scripts for data generation, handling CI/CD pipelines, working with technical product owners to ensure best practices and work on development of features and bug fixes. I'm proficient with Linux, predominantly Arch and Ubuntu. I am good in Javascript and Python  <br />
+          I enage with the cloud using the sdk and deal with various AWS services mainly; <a target="_blank" href="https://aws.amazon.com/ecs/"> ECS</a>, <a target="_blank" href="https://aws.amazon.com/lambda/"> Lambdas</a>,  <a target="_blank"  href="https://aws.amazon.com/cloudformation/"> Cloudformation,</a> <a target="_blank"  href="https://aws.amazon.com/sns/"> SNS</a>, <a target="_blank"  href="https://aws.amazon.com/sqs/"> SQS</a>,<a target="_blank"  href="https://aws.amazon.com/cloudwatch/"> Cloudwatch</a>, <a target="_blank" href="https://aws.amazon.com/step-functions/"> Step functions</a>, <a target="_blank" href="https://aws.amazon.com/dynamodb"> DynamoDb</a>, <a target="_blank" href="https://aws.amazon.com/athena"> Athena</a>
+          I have also worked on integration of various messaging plugins like <a target="_blank" href="https://www.twilio.com/"> Twilio</a>, <a target="_blank" href="https://www.sendgrid.com/"> SendGrid</a>, <a target="_blank" href="https://www.cheetahmail.com/"> Cheetah Mail</a>, <span>etc</span> 
+          <br />Check out my{' '}
           <Scrollchor to="#projects">projects</Scrollchor> below.
         </div>
       </div>
